ab14ded455b22ea1d987196ccfea47daab94bc8e
Jeffery Mutuku <136019231+TremerYT@users.noreply.github.com>
2023-11-17 12:29:18 +0300

    Update node_queue.c

--- node_queue.c
+++ node_queue.c
@@ -4,7 +4,7 @@
  * @header: This is the header of the stack
  * @i: This is the line position number
 */
-void montyQueue(stack_t **head, unsigned int i)
+void node_queue(stack_t **head, unsigned int i)
 {
 	(void)head;
 	(void)i;
